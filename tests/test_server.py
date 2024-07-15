3da7f195b61585618f75d215ca755741cf660d76
Steve <mas.ste@gmail.com>
2024-07-15 14:48:05 +0200

    Fixed purchase when insufficiant club points

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,11 +3,8 @@ from server import loadCompetitions, loadClubs
 
 def test_purchase_places(client):
 
-    clubs = loadClubs()
-    competitions = loadCompetitions()
-
-    test_club = clubs[0]
-    test_competition = competitions[0]
+    test_club = loadClubs()[0]
+    test_competition = loadCompetitions()[0]
     places_to_purchase = 8
 
     response = client.post('/purchasePlaces', data={
@@ -22,11 +19,8 @@ def test_purchase_places(client):
 
 def test_max_purchase_places(client):
 
-    clubs = loadClubs()
-    competitions = loadCompetitions()
-
-    test_club = clubs[0]
-    test_competition = competitions[0]
+    test_club = loadClubs()[0]
+    test_competition = loadCompetitions()[0]
     places_to_purchase = 28
 
     response = client.post('/purchasePlaces', data={
@@ -37,3 +31,19 @@ def test_max_purchase_places(client):
 
     assert response.status_code == 200
     assert b'Max purchase 12.' in response.data
+
+
+def test_has_sufficient_points(client):
+
+    test_club = loadClubs()[1]
+    test_competition = loadCompetitions()[0]
+    places_to_purchase = 9
+
+    response = client.post('/purchasePlaces', data={
+        'club': test_club['name'],
+        'competition': test_competition['name'],
+        'places': places_to_purchase
+    })
+
+    assert response.status_code == 200
+    assert b'Insufficiant points.' in response.data
