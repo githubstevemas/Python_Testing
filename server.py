6a03829d0cf58a48228d96ad120c20b7f858d680
Steve <mas.ste@gmail.com>
2024-07-15 13:50:33 +0200

    Fixed max purchase limit

--- server.py
+++ server.py
@@ -52,10 +52,18 @@ def purchasePlaces():
     competition = \
     [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
+
     placesRequired = int(request.form['places'])
+
+    if placesRequired > 12:
+        flash('Max purchase 12.')
+        return render_template('welcome.html', club=club,
+                               competitions=competitions)
+
     competition['numberOfPlaces'] = int(
         competition['numberOfPlaces']) - placesRequired
     flash('Great-booking complete!')
+
     return render_template('welcome.html', club=club,
                            competitions=competitions)
 
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,5 +1,39 @@
+from server import loadCompetitions, loadClubs
 
 
-class PurchasePlaces:
-    def test_purchase_places(self):
-        assert False
+def test_purchase_places(client):
+
+    clubs = loadClubs()
+    competitions = loadCompetitions()
+
+    test_club = clubs[0]
+    test_competition = competitions[0]
+    places_to_purchase = 8
+
+    response = client.post('/purchasePlaces', data={
+        'club': test_club['name'],
+        'competition': test_competition['name'],
+        'places': places_to_purchase
+    })
+
+    assert response.status_code == 200
+    assert b'Great-booking complete!' in response.data
+
+
+def test_max_purchase_places(client):
+
+    clubs = loadClubs()
+    competitions = loadCompetitions()
+
+    test_club = clubs[0]
+    test_competition = competitions[0]
+    places_to_purchase = 28
+
+    response = client.post('/purchasePlaces', data={
+        'club': test_club['name'],
+        'competition': test_competition['name'],
+        'places': places_to_purchase
+    })
+
+    assert response.status_code == 200
+    assert b'Max purchase 12.' in response.data
